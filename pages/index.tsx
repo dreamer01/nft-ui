e225e04b486518e277a1d3d20b17fa8d59a4f7f9
Pushpendra Singh <psnitsri01@gmail.com>
2022-08-22 17:27:52 +0530

    Bid Modal Update

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,10 +7,14 @@ import Header from '../components/Header';
 import Token from '../components/Token';
 import Bid from '../components/Bid';
 
+const tokensR1 = ['n-kobe', 'n-harden', 'n-durant'];
+
+const tokensR2 = ['n-irvin', 'n-anteto', 'n-harden-2'];
+
 const Home: NextPage = () => {
   const [showBid, toggleBid] = useState(false);
   const [selectedToken, selectToken] = useState('');
-  const [price, setPrice] = useState({});
+  const [price, setPrice] = useState(null);
 
   useEffect(() => {
     let prices = window.localStorage.getItem('tokensPrice');
@@ -41,117 +45,52 @@ const Home: NextPage = () => {
       </h1>
       <main>
         <section className='flex justify-between mt-10'>
-          <div className='bg-slate-200 w-1/4 drop-shadow-lg	rounded-sm border-1'>
-            <Token src='/img/tokens/n-kobe.png' />
-            <div className='m-2 flex justify-between'>
-              <p className='text-2xl font-bold'>{price['n-kobe']} ETH</p>
-              <p className='text-xl text-neutral-500'>ox8247587#####</p>
+          {tokensR1.map((token) => (
+            <div className='bg-slate-200 w-1/4 drop-shadow-lg	rounded-sm border-1'>
+              <Token src={`/img/tokens/${token}.png`} />
+              <div className='m-2 flex justify-between'>
+                <p className='text-2xl font-bold'>
+                  {price && price[token]} ETH
+                </p>
+                <p className='text-xl text-neutral-500'>ox8247587#####</p>
+              </div>
+              <div className='flex justify-center items-center m-4 hover:bg-red-600 hover:text-white'>
+                <button
+                  className=' border-2 border-red-600 w-full'
+                  onClick={() => {
+                    toggleBid(true);
+                    selectToken(token);
+                  }}
+                >
+                  Bid
+                </button>
+              </div>
             </div>
-            <div className='flex justify-center items-center m-4 hover:bg-red-600 hover:text-white'>
-              <button
-                className=' border-2 border-red-600 w-full'
-                onClick={() => {
-                  toggleBid(true);
-                  selectToken('n-kobe');
-                }}
-              >
-                Bid
-              </button>
-            </div>
-          </div>
-          <div className='bg-slate-200 w-1/4 drop-shadow-lg	rounded-sm border-1'>
-            <Token src='/img/tokens/n-harden.png' />
-            <div className='m-2 flex justify-between'>
-              <p className='text-2xl font-bold'>{price['n-harden']} ETH</p>
-              <p className='text-xl text-neutral-500'>ox8247587#####</p>
-            </div>
-            <div className='flex justify-center items-center m-4 hover:bg-red-600 hover:text-white'>
-              <button
-                className=' border-2 border-red-600 w-full'
-                onClick={() => {
-                  toggleBid(true);
-                  selectToken('n-harden');
-                }}
-              >
-                Bid
-              </button>
-            </div>
-          </div>
-          <div className='bg-slate-200 w-1/4 drop-shadow-lg	rounded-sm border-1'>
-            <Token src='/img/tokens/n-durant.png' />
-            <div className='m-2 flex justify-between'>
-              <p className='text-2xl font-bold'>0.3 ETH</p>
-              <p className='text-xl text-neutral-500'>ox8247587#####</p>
-            </div>
-            <div className='flex justify-center items-center m-4 hover:bg-red-600 hover:text-white'>
-              <button
-                className=' border-2 border-red-600 w-full'
-                onClick={() => {
-                  toggleBid(true);
-                  selectToken('n-durant');
-                }}
-              >
-                Bid
-              </button>
-            </div>
-          </div>
+          ))}
         </section>
-
         <section className='flex justify-between mt-10'>
-          <div className='bg-slate-200 w-1/4 drop-shadow-lg	rounded-sm border-1'>
-            <Token src='/img/tokens/n-irvin.png' />
-            <div className='m-2 flex justify-between'>
-              <p className='text-2xl font-bold'>0.3 ETH</p>
-              <p className='text-xl text-neutral-500'>ox8247587#####</p>
+          {tokensR2.map((token) => (
+            <div className='bg-slate-200 w-1/4 drop-shadow-lg	rounded-sm border-1'>
+              <Token src={`/img/tokens/${token}.png`} />
+              <div className='m-2 flex justify-between'>
+                <p className='text-2xl font-bold'>
+                  {price && price[token]} ETH
+                </p>
+                <p className='text-xl text-neutral-500'>ox8247587#####</p>
+              </div>
+              <div className='flex justify-center items-center m-4 hover:bg-red-600 hover:text-white'>
+                <button
+                  className=' border-2 border-red-600 w-full'
+                  onClick={() => {
+                    toggleBid(true);
+                    selectToken(token);
+                  }}
+                >
+                  Bid
+                </button>
+              </div>
             </div>
-            <div className='flex justify-center items-center m-4 hover:bg-red-600 hover:text-white'>
-              <button
-                className=' border-2 border-red-600 w-full'
-                onClick={() => {
-                  toggleBid(true);
-                  selectToken('n-irvin');
-                }}
-              >
-                Bid
-              </button>
-            </div>
-          </div>
-          <div className='bg-slate-200 w-1/4 drop-shadow-lg	rounded-sm border-1'>
-            <Token src='/img/tokens/n-anteto.png' />
-            <div className='m-2 flex justify-between'>
-              <p className='text-2xl font-bold'>0.3 ETH</p>
-              <p className='text-xl text-neutral-500'>ox8247587#####</p>
-            </div>
-            <div className='flex justify-center items-center m-4 hover:bg-red-600 hover:text-white'>
-              <button
-                className=' border-2 border-red-600 w-full'
-                onClick={() => {
-                  toggleBid(true);
-                  selectToken('n-anteto');
-                }}
-              >
-                Bid
-              </button>
-            </div>
-          </div>
-          <div className='bg-slate-200 w-1/4 drop-shadow-lg	rounded-sm border-1'>
-            <Token src='/img/tokens/n-harden-2.png' />
-            <div className='m-2 flex justify-between'>
-              <p className='text-2xl font-bold'>0.3 ETH</p>
-              <p className='text-xl text-neutral-500'>ox8247587#####</p>
-            </div>
-            <div className='flex justify-center items-center m-4 hover:bg-red-600 hover:text-white'>
-              <button
-                className=' border-2 border-red-600 w-full'
-                onClick={() => {
-                  toggleBid(true);
-                  selectToken('n-harden-2');
-                }}
-              >
-                Bid
-              </button>
-            </div>
-          </div>
+          ))}
         </section>
       </main>
       {showBid && (
